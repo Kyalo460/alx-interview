364910bd7f9ce68ae5147dcb865b4b4980d7dcbb
Kyalo460 <Kyalo460@gmail.com>
2024-08-09 12:02:39 +0300

    pushing files

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+def pascal_triangle(n):
+    arr = []
+
+    if n <= 0:
+        return arr
+
+    for i in range(n):
+        inner_arr = []
+        for j in range(i+1):
+            if i == 0:
+                inner_arr.append(1)
+            elif j == 0:
+                inner_arr.append(1)
+            elif j == i:
+                inner_arr.append(1)
+            else:
+                num = arr[i - 1][j - 1] + arr[i - 1][j]
+                inner_arr.append(num)
+
+        arr.append(inner_arr)
+
+    return arr
