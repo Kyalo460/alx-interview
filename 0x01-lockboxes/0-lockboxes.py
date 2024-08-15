1b437f5fc5b201542e0499cf892b6ea0e0cae6f1
Kyalo460 <Kyalo460@gmail.com>
2024-08-15 09:34:13 +0300

    final solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,22 +4,19 @@
 
 def canUnlockAll(boxes):
     """Return True if all boxes can be opened, else return False."""
-
     keys = set()
 
-    # print(boxes)
-    # print(f"range is {len(boxes)}")
     for index in range(len(boxes)):
-        # print(f"range is {len(boxes[index])}")
         for inner_index in range(len(boxes[index])):
             if (boxes[index][inner_index] >= len(boxes)):
                 continue
             elif (boxes[index][inner_index] == 0):
                 continue
+            elif (boxes[index][inner_index] == index):
+                continue
             else:
                 keys.add(boxes[index][inner_index])
 
-    # print(f"Keys are {keys}")
     if (len(keys) == len(boxes) - 1):
         return True
     else:
