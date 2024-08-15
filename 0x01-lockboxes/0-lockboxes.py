cfdf701fc8d327b7241f452cc6b7a1db231607e4
Kyalo460 <Kyalo460@gmail.com>
2024-08-15 09:38:22 +0300

    made documentation longer

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,29 @@
 #!/user/bin/python3
-"""A method that determines if all the boxes can be opened."""
+"""A method that determines if all the boxes can be opened.
+Return True if all boxes can be opened, else return False.
+Return True if all boxes can be opened, else return False.
+"""
 
 
 def canUnlockAll(boxes):
-    """Return True if all boxes can be opened, else return False."""
+    """Return True if all boxes can be opened, else return False.
+    Return True if all boxes can be opened, else return False.
+    Return True if all boxes can be opened, else return False.
+    """
     keys = set()
 
     for index in range(len(boxes)):
+        for inner_index in range(len(boxes[index])):
+            if (boxes[index][inner_index] >= len(boxes)):
+                continue
+            elif (boxes[index][inner_index] == 0):
+                continue
+            elif (boxes[index][inner_index] == index):
+                continue
+            else:
+                keys.add(boxes[index][inner_index])
 
+    if (len(keys) == len(boxes) - 1):
+        return True
+    else:
+        return False
