29c2b5db612a616bdf259e5e0a984a9e3d6bcc42
Kyalo460 <Kyalo460@gmail.com>
2024-08-20 07:20:07 +0300

    found the prime factors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,4 +9,24 @@ def minOperations(n):
     Returns 0 if not possible.
     """
 
-    return 0
+    if (n <= 1):
+        return 0
+
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+
+    for i in range(3, int(n**0.5) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+
+    if n > 2:
+        factors.append(n)
+
+    sum = 0
+    for num in factors:
+        sum += num
+
+    return sum
