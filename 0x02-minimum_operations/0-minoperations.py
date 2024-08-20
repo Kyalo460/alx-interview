8d6878e9ffe3c3c6a8de5b7703c9881b92c8e4b5
Kyalo460 <Kyalo460@gmail.com>
2024-08-20 06:00:13 +0300

    Pushing files

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhton3
+"""a method that calculates the fewest number of operations needed to
+ result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    """Returns the number of operations if possible
+    Returns 0 if not possible.
+    """
+
+    return 0
