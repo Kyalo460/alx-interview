1aa68f449d0c555593a0bf091c45f8d6f30c0093
Kyalo460 <Kyalo460@gmail.com>
2024-08-28 13:12:43 +0300

    removed whitelines

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,11 +8,9 @@ import signal
 
 def handle_sigint(signum, frame):
     print("File size:", total_size)
-
     ordered_dict = dict(sorted(status_codes_dict.items()))
     for key, value in ordered_dict.items():
         print(f"{key}: {value}")
-
 signal.signal(signal.SIGINT, handle_sigint)
 
 count = 0
@@ -22,31 +20,23 @@ status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 status_codes_dict = {}
 for line in sys.stdin:
     count += 1
-
     # Checks if input matches a pattern and parses it
     pattern = r'(\d+\.\d+\.\d+\.\d+)\s-\s\[(.*?)\]\s"(.*?)"\s(\d+)\s(\d+)'
-
     match = re.match(pattern, line)
-
     if match is None:
         continue
-
     status_code = match.group(4)
     file_size = match.group(5)
-
     total_size += int(file_size)
-
     # Checks if status code is in the list of status codes
     if int(status_code) in status_codes:
         if status_code in status_codes_dict:
             status_codes_dict[status_code] += 1
         else:
             status_codes_dict[status_code] = 1
-
     if count == cap:
         cap += 10
         print("File size:", total_size)
-
         ordered_dict = dict(sorted(status_codes_dict.items()))
         for key, value in ordered_dict.items():
             print(f"{key}: {value}")
