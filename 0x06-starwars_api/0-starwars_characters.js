77fa9206c376a79fc169cf63827ddef411531a79
Kyalo460 <Kyalo460@gmail.com>
2024-09-19 05:33:32 +0300

    removed commented code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,5 @@
 #!/usr/bin/node
 
-// import fetch from 'node-fetch';
-
-// const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
-// fetch(url)
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(data => {
-//     data.characters.forEach(character => {
-//       fetch(character)
-//         .then(response => {
-//           return response.json();
-//         })
-//         .then(data => {
-//           console.log(data.name);
-//         })
-//         .catch(error => {
-//           console.log(error);
-//         });
-//     });
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-
 import fetch from 'node-fetch';
 
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
