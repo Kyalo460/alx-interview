a85d89e1b8855bd01f3d5f4eb111a6bb8cccec89
Kyalo460 <Kyalo460@gmail.com>
2024-09-19 05:32:34 +0300

    printed names at the same time

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -34,14 +34,22 @@ const fetchJson = async (url) => {
   if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
   return response.json();
 };
-
+let characters = "";
+let count = 0;
 const main = async () => {
   try {
     const filmData = await fetchJson(url);
     for (const characterUrl of filmData.characters) {
       const characterData = await fetchJson(characterUrl);
-      console.log(characterData.name);
+      // console.log(characterData.name);
+      if (count === 0) {
+        characters += characterData.name
+        count += 1;
+      } else {
+        characters += `\n${characterData.name}`
+      }
     }
+    console.log(characters);
   } catch (error) {
     console.error('Error:', error);
   }
