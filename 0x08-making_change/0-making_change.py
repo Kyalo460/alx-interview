98c164807da8fb68a8343dcc6339d8c490b75523
Kyalo460 <Kyalo460@gmail.com>
2024-10-03 16:01:55 +0300

    pushing files

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""determines the fewest number of coins needed to meet a given amount total"""
+
+
+def makeChange(coins, total):
+    """
+    coins - a list of coins
+    total - the total to be met
+    Return: fewest number of coins needed to meet total
+            0 if total is 0
+            -1 if total can't be met
+    """
+    if total == 0:
+        return 0
+
+    coins_total = 0
+
+    for coin in coins:
+        coins_total += coin
+
+    coins.sort()
+    n = len(coins)
+    i = n - 1
+    count = 0
+
+    while(i >= 0) :
+        while(total >= coins[i]):
+            total -= coins[i]
+            count += 1
+
+        i -= 1
+
+    if total == 0:
+        return count
+    else:
+        return -1
